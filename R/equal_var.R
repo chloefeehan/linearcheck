42d1171e0f477cc1db67a3689f9a0aadf349b511
chloefeehan <chloefeehan29@gmail.com>
2024-05-17 16:42:57 -0700

    adding commentary function

--- R/equal_var.R
+++ R/equal_var.R
@@ -32,20 +32,39 @@ residplot <- function(data, x, y) {
 }
 
 
-bptable <- function(data, x, y) {
+bptable <- function(data, x, y, commentary = NULL) {
   bp_values <- bptest(y ~ x, data = data)
   bp_table <- data.frame(
     BP = bp_values$statistic,
     df = bp_values$parameter,
-    pvalue = bp_values$p.value
+    p_value = bp_values$p.value
   )
-
   table <- kable(bp_table, "html", caption = "Breusch-Pagan Test") |>
     kable_styling(full_width = FALSE)
   print(table)
+
+  if (is.null(commentary)) {
+
+    commentary <- equalvar_commentary(data, x, y)
+    cat(commentary)
+  }
 }
 
 
-bptable(data, x, y)
+equalvar_commentary <- function(data, x, y) {
+  bptable(data, x, y)
+    if (bp_values$p.value > 0.05) {
+      commentary <- cat("There is significant evidence to conclude there is homoscedasticity
+                        in the model because the p-value:",bp_values$p.value,"is greater
+                        than 0.05. This passes the equal variance assumption.")
+    } else {
+      commentary <- cat("There is significant evidence to conclude there is heteroscedasticity
+                        in the model because the p-value:",bp_values$p.value,"is less
+                        than or equal to 0.05. This violates the equal variance assumption.")
+    }
+  return(commentary)
+}
 
+bptable(data, x, y, commentary = TRUE)
+equalvar_commentary(data, x, y)
 data <- data.frame(x = c(1, 2, 3, 4, 5, 6), y = c(2, 4, 7, 10, 11, 14))
