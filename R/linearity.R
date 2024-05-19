bb31962bd462e037e5556c65c6e9faf8e913a49a
evadykaar <erdykaar@calpoly.edu>
2024-05-19 16:19:20 -0700

    Deleted original commentary function

--- R/linearity.R
+++ R/linearity.R
@@ -24,10 +24,6 @@ linearplot <- function(data, x, y, title, commentary = NULL) {
   print(plot)
 }
 
-# commentary <- function() {
-#  if
-# }
-
 
 #data <- data.frame(x = c(1, 2, 3, 4, 5, 6), y = c(2, 4, 7, 10, 11, 14))
 
