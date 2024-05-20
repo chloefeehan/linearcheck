0b0c7bea6175708b73fdfa4f1dc52f9f06ec280c
chloefeehan <chloefeehan29@gmail.com>
2024-05-19 17:40:01 -0700

    adding commentary for linearity check

--- R/linearity.R
+++ R/linearity.R
@@ -12,10 +12,10 @@
 #'
 #' @export
 
-linearplot <- function(data, x, y, title, commentary = NULL) {
-  library(ggplot2)
-  library(tidyverse)
+library(ggplot2)
+library(tidyverse)
 
+linearplot <- function(data, x, y, title) {
   plot <- ggplot(data, mapping = aes(x = {{x}}, y = {{y}})) +
     geom_point(color = "blue") +
     geom_smooth(method = "lm", fill = "lightblue", color = "aquamarine4") +
@@ -24,23 +24,78 @@ linearplot <- function(data, x, y, title, commentary = NULL) {
   print(plot)
 }
 
+stats_table <- function(model, commentary = NULL) {
+  model_summary <- summary(model)
+  print(model_summary)
 
-linearity_commentary <- function(bp_values) {
-  if (bp_values$p.value > 0.05) {
-    commentary <- paste0("There is significant evidence to conclude there is homoscedasticity",
-                         "in the model because the p-value: ", round(bp_values$p.value, 4),
-                         " is greater than 0.05. This passes the equal variance assumption.")
-  } else {
-    commentary <- paste("There is significant evidence to conclude there is heteroscedasticity",
-                        "in the model because the p-value: ", round(bp_values$p.value, 4),
-                        " is less than or equal to 0.05. This violates the equal variance assumption.")
+  if (commentary == TRUE) {
+    commentary <- linearity_commentary(model)
+    cat("\nCommentary: ", commentary, "\n")
   }
-  return(commentary)
+}
+
+linearity_commentary <- function(model) {
+  model_summary <- summary(model)
+  adj_rsquared <- model_summary$adj.r.squared
+  p_value <- model_summary$fstatistic["Pr(>F)"] ### GET P_VALUE
+  if (adj_rsquared >= 0.8) {
+    r <- paste0("The adjusted r-squared value is ", round(adj_rsquared, 4),
+                         ". Since ", round(adj_rsquared, 4),
+                         " is greater than or equal to 0.8, this demonstrates a strong positive",
+                         " relationship between the predictor and response variables.")
+    } else if (adj_rsquared >= 0.5 & adj_rsquared < 0.8) {
+      r <- paste0("The adjusted r-squared value is ", round(adj_rsquared, 4),
+                           ". Since ", round(adj_rsquared, 4),
+                           " is between 0.5 and 0.8, this demonstrates a moderate positive",
+                           " relationship between the predictor and response variables.")
+  } else if (adj_rsquared > 0 & adj_rsquared < 0.5) {
+    r <- paste0("The adjusted r-squared value is ", round(adj_rsquared, 4),
+                         ". Since ", round(adj_rsquared, 4),
+                           " is between 0 and 0.5, this demonstrates a weak positive",
+                           " relationship between the predictor and response variables.")
+  } else if (adj_rsquared > -0.5 & adj_rsquared < 0) {
+    r <- paste0("The adjusted r-squared value is ", round(adj_rsquared, 4),
+                         ". Since ", round(adj_rsquared, 4),
+                         " is between 0 and -0.5, this demonstrates a weak negative",
+                         " relationship between the predictor and response variables.")
+  } else if (adj_rsquared <= -0.5 & adj_rsquared > -0.8) {
+    r <- paste0("The adjusted r-squared value is ", round(adj_rsquared, 4),
+                         ". Since ", round(adj_rsquared, 4),
+                         " is between -0.5 and -0.8, this demonstrates a weak negative",
+                         " relationship between the predictor and response variables.")
+  } else if (adj_rsquared <= -0.8) {
+    r <- paste0("The adjusted r-squared value is ", round(adj_rsquared, 4),
+                         ". Since ", round(adj_rsquared, 4),
+                         " is less than or equal to -0.8, this demonstrates a strong negative",
+                         " relationship between the predictor and response variables.")
+  } else if (adj_rsquared == 0) {
+    r <- paste0("The adjusted r-squared value is ", round(adj_rsquared, 4),
+                         ". Since the adjusted",
+                         " r-squared equals zero, this demonstrates no",
+                         " relationship between the predictor and response variables.")
+  }
+  if (p_value > 0.05) {
+    p <- paste("There is significant evidence to conclude there is homoscedasticity",
+                        "in the model because the p-value:",round(p_value, 4),
+                        " is greater than 0.05. This passes the equal variance assumption.")
+  } else {
+    p <- paste("There is significant evidence to conclude there is heteroscedasticity",
+                        "in the model because the p-value:", round(p_value, 4),
+                        " is less than or equal to 0.05. This violates the equal variance assumption.")
+
+  }
+
+  return(r, p)
 }
 
 
 
-#data <- data.frame(x = c(1, 2, 3, 4, 5, 6), y = c(2, 4, 7, 10, 11, 14))
 
-#linearplot(data, x, y, "title")
+data <- data.frame(x = c(1, 2, 3, 4, 5, 6), y = c(2, 4, 7, 10, 11, 14))
+model <- lm(y~x, data = data)
+linearity_commentary(model)
+
+linearplot(data, x, y, "title")
+
+#source: https://www.statology.org/adjusted-r-squared-in-r/
 
